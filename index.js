4aa7562e9bff65445dbcee38d3428cce5d4a29d4
developerhr000 <80412774+developerhr000@users.noreply.github.com>
2022-05-25 11:10:27 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -4,9 +4,43 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { get } = require("express/lib/response");
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.q5tbl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    client.connect();
+    const productCollection = client
+      .db("assignment-12")
+      .collection("allProducts");
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = await productCollection.find(query).toArray();
+      res.send(cursor);
+    });
+
+    app.post("/products", async (req, res) => {
+      const data = req.body;
+      const result = await productCollection.insertOne(data);
+      res.send(result);
+    });
+    console.log("connect");
+  } finally {
+  }
+};
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
